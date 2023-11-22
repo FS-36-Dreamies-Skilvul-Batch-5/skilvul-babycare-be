5a2b397efc4c3338b70951691eb32447fae4fad0
Bradley <bradleyandrewercole28@gmail.com>
2023-11-22 11:44:01 +0700

    Add New Baby Nutrition Record Controller

--- routes/baby-route.js
+++ routes/baby-route.js
@@ -3,12 +3,14 @@ const route = express.Router();
 
 const {
   getAllBaby,
+  getBabyNutritionRecords,
   createBaby,
   updateBaby,
   deleteBaby
 } = require("../controllers/baby-controller");
 
 route.get("/", getAllBaby);
+route.get("/:id/nutrition-records", getBabyNutritionRecords);
 route.post("/", createBaby);
 route.put("/:id", updateBaby);
 route.delete("/:id", deleteBaby);
