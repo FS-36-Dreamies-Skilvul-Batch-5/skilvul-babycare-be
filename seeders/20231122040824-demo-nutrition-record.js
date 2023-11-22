a981e18d3d045a5a139cbce513bf0a8bf0c63cea
Bradley <bradleyandrewercole28@gmail.com>
2023-11-22 22:23:22 +0700

    Remove Comment

--- seeders/20231122040824-demo-nutrition-record.js
+++ seeders/20231122040824-demo-nutrition-record.js
@@ -34,25 +34,3 @@ module.exports = {
     await queryInterface.bulkDelete("Nutrition_Records", null, {});
   }
 };
-
-// {
-//   "id": 1,
-//   "user_id": 2,
-//   "name": "Jaden",
-//   "gender": "male",
-//   "birth_date": "2023-05-10"
-// },
-// {
-//   "id": 2,
-//   "user_id": 3,
-//   "name": "Brenda",
-//   "gender": "female",
-//   "birth_date": "2023-06-15"
-// },
-// {
-//   "id": 3,
-//   "user_id": 4,
-//   "name": "Galeway",
-//   "gender": "male",
-//   "birth_date": "2023-08-24"
-// }
