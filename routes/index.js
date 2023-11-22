03ccf0559da485f98e9de0e0caf8d573749a4c95
Bradley <bradleyandrewercole28@gmail.com>
2023-11-22 10:46:33 +0700

    Enabling Vaccine Route

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const userRoutes = require("./user-route");
 const babyRoutes = require("./baby-route");
 // const nutritionRecordRoutes = require("./nutrition-record-route");
 // const vaccinationRecordRoutes = require("./vaccination-record-route");
-// const vaccineRoutes = require("./vaccine-route");
+const vaccineRoutes = require("./vaccine-route");
 const articleRoutes = require("./article-route");
 const articleCategoryRoutes = require("./article-category-route");
 
@@ -20,7 +20,7 @@ route.use("/users", userRoutes);
 route.use("/babies", babyRoutes);
 // route.use("/nutrition-records", nutritionRecordRoutes);
 // route.use("/vaccination-records", vaccinationRecordRoutes);
-// route.use("/vaccines", vaccineRoutes);
+route.use("/vaccines", vaccineRoutes);
 route.use("/articles", articleRoutes);
 route.use("/article-categories", articleCategoryRoutes);
 
