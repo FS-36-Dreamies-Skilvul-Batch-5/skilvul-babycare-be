dd5dd2b65d6d6ad0b6bbaa07f6b02d092937f730
Bradley <bradleyandrewercole28@gmail.com>
2023-11-27 21:56:45 +0700

    Add VerifyTOken Auth for this 3 Routes

--- routes/baby-route.js
+++ routes/baby-route.js
@@ -9,12 +9,13 @@ const {
   updateBaby,
   deleteBaby
 } = require("../controllers/baby-controller");
+const verifyToken = require("../middlewares/auth");
 
-route.get("/", getAllBaby);
-route.get("/:id/nutrition-records", getBabyNutritionRecords);
-route.get("/:id/vaccination-records", getBabyVaccinationRecords);
-route.post("/", createBaby);
-route.put("/:id", updateBaby);
-route.delete("/:id", deleteBaby);
+route.get("/", verifyToken, getAllBaby);
+route.get("/:id/nutrition-records", verifyToken, getBabyNutritionRecords);
+route.get("/:id/vaccination-records", verifyToken, getBabyVaccinationRecords);
+route.post("/", verifyToken, createBaby);
+route.put("/:id", verifyToken, updateBaby);
+route.delete("/:id", verifyToken, deleteBaby);
 
 module.exports = route;
