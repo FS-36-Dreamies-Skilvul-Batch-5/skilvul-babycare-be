d3bdaceb22fbc966a8047c8032764db6595e4f1f
Bradley <bradleyandrewercole28@gmail.com>
2023-11-30 15:01:30 +0700

    Make new Routes for Accesing Images from Multer

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const route = express.Router();
+const path = require("path");
+const fs = require("fs");
 const authRoutes = require("./auth-route");
 const userRoutes = require("./user-route");
 const babyRoutes = require("./baby-route");
@@ -9,10 +11,14 @@ const vaccineRoutes = require("./vaccine-route");
 const articleRoutes = require("./article-route");
 const articleCategoryRoutes = require("./article-category-route");
 
+const basePath = path.join(__dirname, '..');
+const publicArticlesPath = path.join(basePath, 'public/articles');
+const publicUsersPath = path.join(basePath, 'public/users');
+
 route.get("/", (req, res) => {
   res.json({
     message: "Hello!, This is FS-36 BabyCare's Endpoint"
-  })
+  });
 });
 
 route.use("/auth", authRoutes);
@@ -23,5 +29,26 @@ route.use("/vaccination-records", vaccinationRecordRoutes);
 route.use("/vaccines", vaccineRoutes);
 route.use("/articles", articleRoutes);
 route.use("/article-categories", articleCategoryRoutes);
+
+route.get('/article-image/:imageName', (req, res) => {
+  const imageName = req.params.imageName;
+  const imagePath = path.join(publicArticlesPath, imageName);
+
+  if (fs.existsSync(imagePath)) {
+    res.sendFile(imagePath);
+  } else {
+    res.status(404).send('File not found');
+  }
+});
+route.get('/user-image/:imageName', (req, res) => {
+  const imageName = req.params.imageName;
+  const imagePath = path.join(publicUsersPath, imageName);
+
+  if (fs.existsSync(imagePath)) {
+    res.sendFile(imagePath);
+  } else {
+    res.status(404).send('File not found');
+  }
+});
 
 module.exports = route;
